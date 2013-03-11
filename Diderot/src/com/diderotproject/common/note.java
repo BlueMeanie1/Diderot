cbe8d69348616ebd667d104c0c3493338ee351b4
jzeidner <jzeidner@W7151777.its.itsservices.com>
2013-03-11 16:14:25 -0400

    added a few classes

---
+++ Diderot/src/com/diderotproject/common/note.java
@@ -0,0 +1,5 @@
+package com.diderotproject.common;
+
+public class note {
+
+}
---
+++ Diderot/src/com/diderotproject/server/DiderotExchangeProtocol_0_1.java
@@ -0,0 +1,17 @@
+package com.diderotproject.server;
+
+import java.security.PublicKey;
+
+import com.diderotproject.common.note;
+import com.diderotproject.common.serverResponse;
+import com.diderotproject.common.trustList;
+
+public interface DiderotExchangeProtocol_0_1 {
+	
+	serverResponse RegisterNym( PublicKey key, String name );
+	
+	serverResponse QueryKeyword( String keyword, trustList tl );
+	
+	serverResponse SubmitNote( note n );
+
+}
